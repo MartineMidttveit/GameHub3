f5773e765b6d53958df225d345230a34de3b2596
Martine23FT <martinemidttveit@hotmail.com>
2023-08-15 18:25:31 +0200

    Changes

---
+++ JS/frontpage.js
@@ -0,0 +1,68 @@
+/// Slideshow ///
+
+var slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slideshow-container")[0].getElementsByClassName("img");
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 4000);
+}
+
+/// MOST POPULAR ///
+
+async function getFour () {
+  const loadGames = await fetch("https://api.noroff.dev/api/v1/gamehub"); 
+  const games = await loadGames.json();
+  let displayMostPopular = "";
+  let displayNewReleases = "";
+
+
+  for (let index = 0; index <= 3; index++) { 
+    displayMostPopular+=
+    `<div class="game-container">
+      <a href="/HTML/gameDetails.html?id=${games[index].id}">
+      <img src="${games[index].image}" alt="Game-covers of listed products"></a>
+      <div class="game-info">
+      <h2 class="game-title">${games[index].title}</h2>
+      <p>${games[index].genre}</p>
+      <p>£ ${games[index].price}</p>
+      </div>
+      <button class="buy-now">BUY NOW</button>
+      </div>`
+    };
+    document.querySelector(".shop-grid").innerHTML = displayMostPopular;
+   
+    const startIndex = games.length - 4;
+
+    if (startIndex >= 0) {
+      let displayNewReleases = "";
+
+    for (let index = startIndex; index < games.length; index++) { 
+      displayNewReleases+=
+      `<div class="game-container">
+        <a href="/HTML/gameDetails.html?id=${games[index].id}">
+        <img src="${games[index].image}" alt="Game-covers of listed products"></a>
+        <div class="game-info">
+        <h2 class="game-title">${games[index].title}</h2>
+        <p>${games[index].genre}</p>
+        <p>£ ${games[index].price}</p>
+        </div>
+        <button class="buy-now">BUY NOW</button>
+        </div>`
+      };
+      document.querySelector("#newReleases").innerHTML = displayNewReleases;
+}};
+
+getFour();
---
+++ JS/shop.js
@@ -0,0 +1,50 @@
+async function getGames() {
+    const queryString = document.location.search;
+    const params = new URLSearchParams(queryString);
+    const genre = params.get("genre");
+
+    try {
+        const loadGames = await fetch("https://api.noroff.dev/api/v1/gamehub");
+        const games = await loadGames.json();
+
+        let display = ""
+
+        games.map((game) => {
+            if (genre && game.genre === genre) {
+                display+= 
+                `<div class="game-container">
+                <a href="/HTML/gameDetails.html?id=${game.id}">
+                <img src="${game.image}" alt="Game-covers of listed products"></a>
+                <div class="game-info">
+                <h2 class="game-title">${game.title}</h2>
+                <p>${game.genre}</p>
+                <p>£ ${game.price}</p>
+                </div>
+                <button class="buy-now">BUY NOW</button>
+                </div>`
+            } 
+            
+            if (!genre) {
+            display+= 
+                `<div class="game-container">
+                <a href="/HTML/gameDetails.html?id=${game.id}">
+                <img src="${game.image}" alt="Game-covers of listed products"></a>
+                <div class="game-info">
+                <h2 class="game-title">${game.title}</h2>
+                <p>${game.genre}</p>
+                <p>£ ${game.price}</p>
+                </div>
+                <button class="buy-now">BUY NOW</button>
+                </div>`
+            }
+        });
+
+        setTimeout(() => {
+            document.querySelector(".games-noroff").innerHTML = display;
+        }, 2000);
+    } catch (error) {
+        throw new Error("Error to fetch the game-data: " + error)
+    }
+}
+
+getGames();
