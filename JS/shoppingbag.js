ac605d942bf2ecf06c334a58ce0260dc055d0ea7
Martine23FT <martinemidttveit@hotmail.com>
2023-08-25 18:59:08 +0200

    Changes

--- JS/shoppingbag.js
+++ JS/shoppingbag.js
@@ -12,38 +12,56 @@ async function getGames (url) {
       const game = await response.json()
 
       gameContainer.innerHTML +=
-    `<h1 class="subtitles">SHOPPINGBAG</h1>
-    <div class="grid">
+        `<h1 class="subtitles">SHOPPINGBAG</h1>
+        <div class="grid">
+            <div class="column"></div>
+
+            <div class="column">
+                <h2>PRODUCT:</h2>
+            </div>
+
+            <div class="column">
+                <h2>QUANTITY:</h2>
+            </div>
+
+            <div class="column">
+                <h2 class="price">PRICE</h2>
+            </div>
+
+            <div class="column">
+                <h2 class="genre">GENRE</h2>
+            </div> 
+            
             <div class="column">
               <figure class="product-image">
                 <img src="${game.image}" alt="">
               </figure>
             </div>
+
             <div class="column">
-                <h2>Product:</h2>
                 <p class="product-name">${game.title}</p>
             </div>
+
             <div class="column">
-                <h2>Quantity:</h2>
                 <form class="quantity">
                   <input type="number">
                 </form>
             </div>
+
             <div class="column">
-                <h2 class="price">Price:</h2>
                 <p class="product-price">${game.price}</p>
             </div>
             <div class="column">
-                <h2 class="devices">Device:</h2>
+                <p class="product-genre">${game.genre}</p>
             </div> 
-    </div>
-    <button class="button scale">PAYMENT</button>`
+        </div>
+        <button class="button scale">PAYMENT</button>`
     } catch (error) {
       throw new Error('Error to load games in the shopping bag: ' + error.message)
     }
   } else {
     gameContainer.innerHTML +=
-    `<h1 class="subtitles">SHOPPINGBAG</h1>`
+    `<h1 class="subtitles">SHOPPINGBAG IS EMPTY</h1>`
   }
 }
 
