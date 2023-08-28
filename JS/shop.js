c26c7a0f13fe0355483a31bcbe9d97079b56f803
Martine23FT <martinemidttveit@hotmail.com>
2023-08-28 21:10:54 +0200

    Changes

--- JS/shop.js
+++ JS/shop.js
@@ -1,54 +1,36 @@
-async function getGames () {
-    const queryString = document.location.search
-    const params = new URLSearchParams(queryString)
-    const genre = params.get('genre')
-  
-    try {
-      const loadGames = await fetch('https://api.noroff.dev/api/v1/gamehub')
-      const games = await loadGames.json()
-  
-      let display = ''
-  
-      games.map((game) => {
-        if (genre && game.genre === genre) {
-          display +=
+async function getGames() {
+  const queryString = document.location.search;
+  const params = new URLSearchParams(queryString);
+  const genre = params.get('genre');
+
+  try {
+      const loadGames = await fetch('https://api.noroff.dev/api/v1/gamehub');
+      const games = await loadGames.json();
+
+      let display = '';
+
+      games.forEach((game) => {
+          if (!genre || game.genre === genre) {
+              display +=
                   `<div class="game-container">
-                  <a href="/HTML/gameDetails.html?id=${game.id}">
-                  <img src="${game.image}" alt="Game-covers of listed products"></a>
-                  <div class="game-info">
-                  <h2 class="game-title">${game.title}</h2>
-                  <p>${game.genre}</p>
-                  <p>£ ${game.price}</p>
-                  </div>
-                  <a href="/HTML/shoppingbag.html?id=${game.id}" class="underline">
+                    <a href="/HTML/gameDetails.html?id=${game.id}">
+                      <img src="${game.image}" alt="Game-covers of listed products"></a>
+                    <div class="game-info">
+                      <h2 class="game-title">${game.title}</h2>
+                      <p>${game.genre}</p>
+                      <p>£ ${game.price}</p>
+                    </div>
+                    <a href="/HTML/shoppingbag.html?id=${game.id}" class="underline">
                       <button class="buy-now">ADD TO CART</button>
-                  </a>
-                  </div>`
-        }
-  
-        if (!genre) {
-          display +=
-                `<div class="game-container">
-                  <a href="/HTML/gameDetails.html?id=${game.id}">
-                  <img src="${game.image}" alt="Game-covers of listed products"></a>
-                  <div class="game-info">
-                  <h2 class="game-title">${game.title}</h2>
-                  <p>${game.genre}</p>
-                  <p>£ ${game.price}</p>
-                  </div>
-                  <a href="/HTML/shoppingbag.html?id=${game.id}" class="underline">
-                      <button class="buy-now">ADD TO CART</button>
-                  </a>
-                  </div>`
-        }
-      })
-  
-      setTimeout(() => {
-        document.querySelector('.games-noroff').innerHTML = display
-      }, 2000)
-    } catch (error) {
-      throw new Error('Error to fetch the game-data: ' + error.message)
-    }
+                    </a>
+                  </div>`;
+          }
+      });
+
+      document.querySelector('.games-noroff').innerHTML = display;
+  } catch (error) {
+      throw new Error('Error fetching game data: ' + error.message);
   }
-  
-  getGames()
+}
+
+getGames();
