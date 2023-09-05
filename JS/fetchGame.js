e31d02fb2cf465ae1c8486174b9f1a4fb30cc598
Martine23FT <martinemidttveit@hotmail.com>
2023-09-05 16:54:17 +0200

    Changes

--- JS/fetchGame.js
+++ JS/fetchGame.js
@@ -1,7 +1,7 @@
 export default async function fetchGames (id = "") {
 
     try {
-        const loadGames = await fetch("https://api.noroff.dev/api/v1/gamehubb/" + id);
+        const loadGames = await fetch("https://api.noroff.dev/api/v1/gamehub/" + id);
         const games = await loadGames.json();
 
         return games;
